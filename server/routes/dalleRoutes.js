ce4639ce59a719cddc771c39545bb9205f0d9488
LucioFider <fidelislucio404@gmail.com>
2023-02-02 13:46:22 +1000

    3rd-commit

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -13,7 +13,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 router.route("/").get((req, res) => {
-  res.send("Hello from Dalle-E!");
+  res.status(200).json({ message: "Hello from DALL-E!" });
 });
 
 router.route("/").post(async (req, res) => {
@@ -26,8 +26,15 @@ router.route("/").post(async (req, res) => {
       size: "1024x1024",
       response_format: "b64_json",
     });
-    res.status;
-  } catch (error) {}
+
+    const image = aiResponse.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
+  }
 });
 
-export 
+export default router;
